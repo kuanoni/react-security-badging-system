eec66fd39fc68aec67077b2cda4539c6feb69ce3
kuanoni <tehgamemaker@gmail.com>
2022-10-26 20:49:10 -0700

    feat: added props parameter to fetchGet

--- src/api/fetch.js
+++ src/api/fetch.js
@@ -1,11 +1,13 @@
 const fetchSize = 30;
 const apiUrl = 'https://security-system-api.herokuapp.com/';
 
-export const fetchGet = async (collection, page, search) => {
+export const fetchGet = async (collection, page, search, props) => {
 	let fetchUrl = apiUrl + collection + '/get';
 
-	if (search.searchBy) fetchUrl += `?searchBy=${search.searchBy}&value=${search.value}`;
+	if (search.filter) fetchUrl += `?filter=${search.filter}&value=${search.value}`;
 	else fetchUrl += '?page=' + (page + 1) + '&limit=' + fetchSize;
+
+	if (props) fetchUrl += '&props=' + props;
 
 	const data = await fetch(fetchUrl, {
 		method: 'GET',
