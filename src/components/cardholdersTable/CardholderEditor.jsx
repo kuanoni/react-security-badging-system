e3df6a60368807d1e4989bd61a8f2b8748c216ae
kuanoni <tehgamemaker@gmail.com>
2022-10-26 21:57:11 -0700

    refactor: removed unnecessary fetch functions

--- src/components/cardholdersTable/CardholderEditor.jsx
+++ src/components/cardholdersTable/CardholderEditor.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import toast from 'react-hot-toast';
-import { fetchGet, fetchUpdate, updateCardholder } from '../../api/fetch';
+import { fetchGet, fetchUpdate } from '../../api/fetch';
 import Modal from '../Modal';
 import LabeledInput from '../forms/LabeledInput';
 import ListAddRemove from '../forms/ListAddRemove';
