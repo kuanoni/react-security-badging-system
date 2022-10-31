2eba8083a8e26cb2f7911e7ab0390f2c4f0036ac
kuanoni <tehgamemaker@gmail.com>
2022-10-31 09:42:16 -0700

    fix: made navbar mobile friendly & responsive

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,46 +3,50 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { fab } from '@fortawesome/free-brands-svg-icons';
 import { faAddressCard, faAnglesLeft, faUser, faUsers } from '@fortawesome/free-solid-svg-icons';
 
-const Navbar = ({ isNavbarOpen, setIsNavbarOpen }) => {
+const Navbar = ({ isNavbarOpen, setIsNavbarOpen, pageName }) => {
 	return (
 		<nav className={isNavbarOpen ? 'open' : 'closed'}>
-			<div className='logo-container'>
-				<span className='text'>SEC</span>
-				<span className='gg-shape-hexagon'></span>
-			</div>
 			<div className='nav-btns-container'>
-				<div className='separator' />
 				<div className='nav-btn nav-toggle-btn' onClick={() => setIsNavbarOpen(!isNavbarOpen)}>
-					<span className='icon-wrapper'>
-						<FontAwesomeIcon icon={faAnglesLeft} />
-					</span>
+					<div className='logo-container'>
+						<span className='text'>SEC</span>
+						<span className='gg-shape-hexagon'></span>
+					</div>
+
+					<div className='nav-btn'>
+						<span className='icon-wrapper'>
+							<FontAwesomeIcon icon={faAnglesLeft} />
+						</span>
+					</div>
 				</div>
 				<div className='separator' />
-				<a href='/' className='nav-btn'>
+				<a href='/' className={'nav-btn' + (pageName === 'cardholders' ? ' active' : '')}>
 					<span className='icon-wrapper'>
 						<FontAwesomeIcon icon={faUser} />
 					</span>
 					<label>Cardholders</label>
 				</a>
-				<a href='credentials' className='nav-btn'>
+				<a href='credentials' className={'nav-btn' + (pageName === 'credentials' ? ' active' : '')}>
 					<span className='icon-wrapper'>
 						<FontAwesomeIcon icon={faAddressCard} />
 					</span>
 					<label>Credentials</label>
 				</a>
-				<a href='access-groups' className='nav-btn'>
+				<a href='access-groups' className={'nav-btn' + (pageName === 'accessGroups' ? ' active' : '')}>
 					<span className='icon-wrapper'>
 						<FontAwesomeIcon icon={faUsers} />
 					</span>
 					<label>Access Groups</label>
 				</a>
-				<div className='separator' />
-				<a href='https://github.com/kuanoni/react-table-security-system' className='nav-btn'>
-					<span className='icon-wrapper'>
-						<FontAwesomeIcon icon={fab.faGithub} />
-					</span>
-					<label>Github</label>
-				</a>
+				<div className='nav-footer'>
+					<div className='separator' />
+					<a href='https://github.com/kuanoni/react-table-security-system' className='nav-btn'>
+						<span className='icon-wrapper'>
+							<FontAwesomeIcon icon={fab.faGithub} />
+						</span>
+						<label>Github</label>
+					</a>
+				</div>
 			</div>
 		</nav>
 	);
