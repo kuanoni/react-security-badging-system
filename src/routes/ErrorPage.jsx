082cc0586653424f081f05eb9d2cc7f5ad7dc719
kuanoni <tehgamemaker@gmail.com>
2022-11-04 07:52:42 -0700

    style: made error page compatible with navbar

--- src/routes/ErrorPage.jsx
+++ src/routes/ErrorPage.jsx
@@ -1,15 +1,17 @@
+import { useState } from 'react';
 import { useRouteError } from 'react-router-dom';
 import Navbar from '../components/Navbar';
 
 export default function ErrorPage() {
+	const [isNavbarOpen, setIsNavbarOpen] = useState(false);
 	const error = useRouteError();
 	console.error(error);
 
 	return (
 		<>
 			<div className='app'>
-				<Navbar />
-				<div className='error-page'>
+				<Navbar isNavbarOpen={isNavbarOpen} setIsNavbarOpen={setIsNavbarOpen} pageName={'error'} />
+				<div className={'error-page' + (isNavbarOpen ? '' : ' navbar-closed')}>
 					<div className='error-container'>
 						<h1>Oops!</h1>
 						<i>Sorry, an unexpected error has occurred.</i>
