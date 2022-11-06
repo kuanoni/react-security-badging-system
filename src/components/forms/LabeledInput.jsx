68614de23cf87bed7328c44f536186943b237e2e
kuanoni <tehgamemaker@gmail.com>
2022-11-05 21:26:33 -0700

    feat(LabeledInput): only displays error when changed

--- src/components/forms/LabeledInput.jsx
+++ src/components/forms/LabeledInput.jsx
@@ -1,18 +1,25 @@
 import React from 'react';
 
-const LabeledInput = ({ label, defaultValue, handleChange, isDisabled }) => {
+const LabeledInput = ({ label, defaultValue, handleChange, errors, isDisabled }) => {
+	const onChange = (e) => {
+		if (e.target.value === '') e.target.className = 'input blank';
+		else e.target.className = 'input';
+
+		handleChange(e.target.value);
+	};
+
 	return (
 		<>
 			<label className='label'>{label}</label>
 			<input
-				// key={dataKey}
-				className='input'
+				className={'input'}
 				type='text'
 				defaultValue={defaultValue}
-				onChange={(e) => handleChange(e.target.value)}
+				onChange={onChange}
 				placeholder={'Enter ' + label.toLowerCase() + '...'}
 				disabled={isDisabled}
 			/>
+			<span className='errors'></span>
 		</>
 	);
 };
