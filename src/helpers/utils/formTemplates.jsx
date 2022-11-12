48eb62daede23039f7fbc81aed5625c554be9284
kuanoni <tehgamemaker@gmail.com>
2022-11-12 08:04:27 -0800

    feat: added character limit to editor inputs

--- src/helpers/utils/formTemplates.jsx
+++ src/helpers/utils/formTemplates.jsx
@@ -6,30 +6,33 @@ import { useAccessGroups, useAvailableCredentials } from '../api/queries';
 
 const lettersField = (val) => {
 	const errors = [];
-	if (val.length === 0) errors.push('This field must not be empty.');
-	if (!/^[A-Za-z\s]*$/.test(val)) errors.push('This field can only contain letters.');
+	if (val.length === 0) errors.push('Must not be empty.');
+	if (val.length > 18) errors.push('Must be under 18 characters.');
+	if (!/^[A-Za-z'\s]*$/.test(val)) errors.push('Must only contain letters.');
 	return errors;
 };
 
 const numbersField = (val) => {
 	const errors = [];
-	if (val.length === 0) errors.push('This field must not be empty.');
-	if (!/^[0-9]*$/.test(val)) errors.push('This field can only contain numbers.');
+	if (val.length === 0) errors.push('Must not be empty.');
+	if (val.length > 18) errors.push('Must be under 18 characters.');
+	if (!/^[0-9]*$/.test(val)) errors.push('Must contain numbers.');
 	return errors;
 };
 
 const lettersNumbersField = (val) => {
 	const errors = [];
-	if (val.length === 0) errors.push('This field must not be empty.');
-	if (!/^[A-Za-z0-9\s]*$/.test(val)) errors.push('This field can only contain letter or numbers.');
+	if (val.length === 0) errors.push('Must not be empty.');
+	if (val.length > 18) errors.push('Must be under 18 characters.');
+	if (!/^[A-Za-z0-9'\s]*$/.test(val)) errors.push('Must contain letter or numbers.');
 	return errors;
 };
 
 const emailField = (val) => {
 	const errors = [];
-	if (val.length === 0) errors.push('This field must not be empty.');
-	if (!/^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/.test(val))
-		errors.push('This field must be a valid email address.');
+	if (val.length === 0) errors.push('Must not be empty.');
+	if (val.length > 40) errors.push('Must be under 40 characters.');
+	if (!/^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/.test(val)) errors.push('Must be a valid email address.');
 	return errors;
 };
 
